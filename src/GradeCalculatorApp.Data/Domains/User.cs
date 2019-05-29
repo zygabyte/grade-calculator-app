a7a6a7d0f8ae1d45bb54d6cbb70e52a69ff3f7c6
ehioghae.eseosa <ehioghae.eseosa@VGG-LT-028>
2019-05-29 13:28:23 +0100

    remove password from user

--- src/GradeCalculatorApp.Data/Domains/User.cs
+++ src/GradeCalculatorApp.Data/Domains/User.cs
@@ -7,7 +7,6 @@ namespace GradeCalculatorApp.Data.Domains
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public string PasswordHash { get; set; }
         public UserRole UserRole { get; set; }
     }
