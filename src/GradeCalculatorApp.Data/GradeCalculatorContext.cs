ad446449e26b9f77bd42234160bb457b223f027f
ehioghae.eseosa <ehioghae.eseosa@VGG-LT-028>
2019-05-29 15:56:22 +0100

    reinitialize context

---
+++ src/GradeCalculatorApp.Data/GradeCalculatorContext.cs
@@ -0,0 +1,20 @@
+using GradeCalculatorApp.Data.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace GradeCalculatorApp.Data
+{
+    public class GradeCalculatorContext : DbContext
+    {
+        public GradeCalculatorContext(DbContextOptions<GradeCalculatorContext> contextOptions)
+        {}
+
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Semester> Semesters { get; set; }
+        public DbSet<School> Schools { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Programme> Programmes { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Lecturer> Lecturers { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
